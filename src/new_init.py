e23aa0639b816f929f0b1778783d9998d1b87f27
Benedict Hildisch <benedict.hildisch@e-sling.com>
2022-12-17 14:07:28 +0100

    Pose to Pose crashed

--- src/new_init.py
+++ src/new_init.py
@@ -33,13 +33,13 @@ class VisualOdometry():
         #     params = np.fromstring(f.readline(), dtype=np.float64, sep=' ')
         #     P = np.reshape(params, (3, 4))
         #     K = P[0:3, 0:3]
-        # K = np.array([[7.188560000000e+02, 0, 6.071928000000e+02],
-        #               [0, 7.188560000000e+02, 1.852157000000e+02],
-        #               [0, 0, 1]])
+        K = np.array([[7.188560000000e+02, 0, 6.071928000000e+02],
+                      [0, 7.188560000000e+02, 1.852157000000e+02],
+                      [0, 0, 1]])
 
-        K = np.array([  [1379.74,   0,          760.35],
-                [    0,     1382.08,    503.41],
-                [    0,     0,          1 ]] )
+        # K = np.array([  [1379.74,   0,          760.35],
+        #         [    0,     1382.08,    503.41],
+        #         [    0,     0,          1 ]] )
 
         base = np.hstack((np.eye(3), np.zeros((3, 1))))
         return K
@@ -136,11 +136,12 @@ class VisualOdometry():
         q1 = q1[q1[:,0] != 0]
         q2 = np.multiply(q2, E_mask)
         q2 = q2[q2[:,0] != 0]
-        pose = cv2.recoverPose(E,q1,q2,self.K,distanceThresh=11)
+        pose = cv2.recoverPose(E,q1,q2,self.K,distanceThresh = 11)
         # pose returns trans from 2 to 1 look at slides!!!
         # Todo: Somt of  here
         R = pose[1].T
         t = R*pose[2]
+        # 21
         trans = np.hstack((pose[1],pose[2]))
         transfrom_mat = np.r_[trans, [np.array([0,0,0,1])]]
         # only retun used q
@@ -164,7 +165,7 @@ class VisualOdometry():
         return triangulated_landmarks[:3]
 
 def main():
-    data_dir = "../data/test/"  # Try KITTI_sequence_2 too
+    data_dir = "../data/kitti/05/image_0"  # Try KITTI_sequence_2 too
     vo = VisualOdometry(data_dir)
 
     #play_trip(vo.images)  # Comment out to not play the trip
@@ -180,10 +181,12 @@ def main():
 
     #for i, gt_pose in enumerate(tqdm(vo.gt_poses, unit="pose")):
     for i,images in enumerate(tqdm(vo.images, unit="images")):
-        if i < 400:
+        if i < 200:
             if i == 0:
                 base = np.hstack((np.eye(3), np.zeros((3, 1))))
+                base = np.r_[base, [np.array([0, 0, 0, 1])]]
                 cur_pose = base
+                cur_pose_back = base
 
             else:
                 K = np.array([[7.188560000000e+02, 0, 6.071928000000e+02],
@@ -193,44 +196,50 @@ def main():
                 transf, q1, q2, triangulatedPoints = vo.get_pose(q1, q2)
                 old_pose = cur_pose
                 cur_pose = np.matmul(cur_pose, np.linalg.inv(transf))
-                transfrom_mat = np.r_[old_pose, [np.array([0, 0, 0, -0.87])]]
-                transfrom_mat_12 = cur_pose #np.r_[cur_pose, [np.array([0, 0, 0, 1])]]
-                transfrom_mat_12[:,3] = np.array([-0.12062195, -0.23368521, 0.96480131])
+                #transfrom_mat = np.r_[old_pose, [np.array([0, 0, 0, 1])]]
+                #transfrom_mat_12 = np.r_[cur_pose, [np.array([0, 0, 0, 1])]]
+                #transfrom_mat_12[:,3] = np.array([-0.12062195, -0.23368521, 0.96480131])
                 print(cur_pose)
                 for i, row in enumerate(triangulatedPoints):
                     #point = np.array([cur_pose[0,3], cur_pose[1,3], cur_pose[2,3],  1]) + row
-                    point = transfrom_mat@row.T
+                    point = old_pose@row.T
                     triangulatedPoints[i] = point.T
                 #triangulatedPoints = vo.triangulate(q1, q2, old_pose, transf)
 
-                for i in range(11): #len(triangulatedPoints)
+                for i in range(triangulatedPoints.shape[0]): #len(triangulatedPoints)
                     # if abs(triangulatedPoints[i,0]) + abs(triangulatedPoints[i,1]) +abs(triangulatedPoints[i,2]) < 100:
                     all_tri.append([triangulatedPoints[i,0],triangulatedPoints[i,1],triangulatedPoints[i,2]])
                     q_pres_1.append([q1[i,0], q1[i,1]])
                     q_pres_2.append([q2[i,0], q2[i,1]])
-                q_pres_1 = np.array(q_pres_1)
-                q_pres_2 = np.array(q_pres_2)
-                triangulatedPoints.astype('double')
-                q1.astype('double')
-                q2.astype('double')
 
-                # back_pose = cv2.solvePnPRansac(triangulatedPoints[:6,:3],q2[:6],K, distCoeffs=np.zeros((4,1)), flags=0)
-                back_pose = cv2.solvePnP(triangulatedPoints[:6,:3],q2[:6],K, distCoeffs=np.zeros((4,1)), flags=0)
-                R = back_pose[1].T
-                t = -back_pose[2]
-                # b_pose = np.hstack((R, t))
-                # b_pose_mat = np.r_[b_pose, [np.array([0, 0, 0, 1])]]
-                all_b_pose.append([t[0], t[1], t[2]])
-            #gt_path.append((gt_pose[0, 3], gt_pose[2, 3]))
-            #estimated_path.append((cur_pose[0, 3], cur_pose[2, 3]))
+                triangulatedPoints = triangulatedPoints[:,:3].astype('float')
+                q1 = q1.astype('float')
+                q2 = q2.astype('float')
+
+                # back_pose = cv2.solvePnP(triangulatedPoints[:6,:3],q2[:6],K, distCoeffs=np.zeros((4,1)), flags=0)
+                #back_pose = cv2.solvePnP(triangulatedPoints[:6,:3],q2[:6],K, None)
+                if triangulatedPoints.shape[0] >= 4:
+                    _, rvec, tvec, _ = cv2.solvePnPRansac(triangulatedPoints, q2, K, None)
+                    R, _ = cv2.Rodrigues(rvec)
+                    # R = -R.T
+                    # t = R@tvec
+                    t = tvec
+                    b_pose = np.hstack((R, t))
+                    transfrom_mat_back = np.r_[b_pose, [np.array([0, 0, 0, 1])]]
+                    transfrom_mat_back = np.linalg.inv(transfrom_mat_back)
+                    #cur_pose_back = np.matmul(cur_pose, np.linalg.inv(transfrom_mat_back))
+                    if abs(transfrom_mat_back[0,3]) + abs(transfrom_mat_back[1,3]) + abs(transfrom_mat_back[2,3]) < 1000:
+                        all_b_pose.append([transfrom_mat_back[0,3], transfrom_mat_back[1,3], transfrom_mat_back[2,3]])
+                #gt_path.append((gt_pose[0, 3], gt_pose[2, 3]))
+                #estimated_path.append((cur_pose[0, 3], cur_pose[2, 3]))
             all_path.append([cur_pose[0, 3], cur_pose[1, 3], cur_pose[2, 3]])
     all_tri = np.array(all_tri)
     reprojected = []
     for elm in all_tri:
-        elm = np.append(elm, 1)
+        #elm = np.append(elm, 1)
         print(elm)
-        su = vo.K@transfrom_mat_12
-        prod = su@elm
+        #su = vo.K@old_pose
+        prod = vo.K@elm
         prod = prod/prod[2]
         print(prod)
         reprojected.append(prod[:2])
@@ -257,9 +266,11 @@ def main():
     ki = all_tri
     kb = np.array(all_b_pose)
     ax.scatter(kk[:,0], kk[:,1], kk[:,2],marker='x')
-    ax.scatter(ki[:,0], ki[:,1], ki[:,2],marker='o')
-    ax.scatter(kb[:, 0], kb[:, 1], kb[:, 2], marker='o')
+    #ax.scatter(ki[:,0], ki[:,1], ki[:,2],marker='o')
+    #ax.scatter(kb[:, 0], kb[:, 1], kb[:, 2], marker='o')
 
+    q_pres_1 = np.array(q_pres_1)
+    q_pres_2 = np.array(q_pres_2)
     ax = fig.add_subplot(1,3,2)
     ax.imshow(vo.images[0])
     ax.scatter(q_pres_1[:,0], q_pres_1[:,1], color = 'y', marker='x')
