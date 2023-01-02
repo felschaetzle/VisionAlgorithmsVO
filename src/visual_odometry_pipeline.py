5140d8b2733f4293cf0f146aeb42fde64d12189e
Jan <egli.jan@gmx.ch>
2023-01-02 15:34:05 +0100

    Add env export and improve names of variables

--- src/visual_odometry_pipeline.py
+++ src/visual_odometry_pipeline.py
@@ -7,7 +7,7 @@ from plotting import Plotter
 
 class VisualOdometryPiper:
 
-    def __init__(self, K, data_dir,number_of_images,plot_live):
+    def __init__(self, K, data_dir, number_of_images, plot_live):
         self.K = K
         self.FLANN_INDEX_LSH = 6
         self.plot_live = plot_live
@@ -29,22 +29,22 @@ class VisualOdometryPiper:
         return [cv2.imread(path, cv2.IMREAD_GRAYSCALE) for path in image_paths]
 
     def visualize(self):
+        found_enough_keypoints = True
         for i,_ in enumerate(tqdm(self.images, unit="images")):
-
+            
             if i == 0:
-                base = np.hstack((np.eye(3), np.zeros((3, 1))))
-                base = np.r_[base, [np.array([0, 0, 0, 1])]]
-                cur_pose = base
+                first_pose = np.hstack((np.eye(3), np.zeros((3, 1))))
+                current_pose = np.r_[first_pose, [np.array([0, 0, 0, 1])]]
 
             else:
                 ### Get Matches ####
                 # Find the keypoints and descriptors with ORB
-                kp1, des1 = self.orb.detectAndCompute(self.images[i - 1], None)
-                kp2, des2 = self.orb.detectAndCompute(self.images[i], None)
+                keypoints_last, descriptor_last = self.orb.detectAndCompute(self.images[i - 1], None)
+                keypoints_current, descriptor_current = self.orb.detectAndCompute(self.images[i], None)
                 # Find matches
-                matches = self.flann.knnMatch(des1, des2, k=2)
+                matches = self.flann.knnMatch(descriptor_last, descriptor_current, k=2)
 
-                # Find the matches that do not have distance which is too high
+                # Filter matches
                 good_matches = []
                 try:
                     for m, n in matches:
@@ -54,8 +54,8 @@ class VisualOdometryPiper:
                     pass
 
                 # Get the image points from the good matches
-                q_last = np.float32([kp1[m.queryIdx].pt for m in good_matches])
-                self.q_current = np.float32([kp2[m.trainIdx].pt for m in good_matches])
+                q_last = np.float32([keypoints_last[m.queryIdx].pt for m in good_matches])
+                self.q_current = np.float32([keypoints_current[m.trainIdx].pt for m in good_matches])
                 self.q_initial = self.q_current
 
                 E, E_mask = cv2.findEssentialMat(q_last, self.q_current, self.K, prob=0.999, threshold=0.45)
@@ -65,61 +65,62 @@ class VisualOdometryPiper:
                 self.q_current = self.q_current[self.q_current[:, 0] != 0]
 
                 # Pose returns transform from 2 to 1 look at slides!!!
+                # Removes disambiguity of E matrix and calculates the pose
                 pose = cv2.recoverPose(E, q_last, self.q_current, self.K, distanceThresh=50)
 
-                transf = np.hstack((pose[1], pose[2]))
-                transf = np.r_[transf, [np.array([0, 0, 0, 1])]]
+                transform_matrix = np.hstack((pose[1], pose[2]))
+                transform_matrix = np.r_[transform_matrix, [np.array([0, 0, 0, 1])]]
 
-                rec_mask = pose[3]
-                rec_mask[rec_mask == 255] = 1
-                q_last = np.multiply(q_last, rec_mask)
+                recover_pose_mask = pose[3]
+                recover_pose_mask[recover_pose_mask == 255] = 1
+                q_last = np.multiply(q_last, recover_pose_mask)
                 q_last = q_last[q_last[:, 0] != 0]
-                self.q_current = np.multiply(self.q_current, rec_mask)
+                self.q_current = np.multiply(self.q_current, recover_pose_mask)
                 self.q_current = self.q_current[self.q_current[:, 0] != 0]
 
-                triangulatedPoints = pose[4]
-                triangulatedPoints = triangulatedPoints / triangulatedPoints[3]
-                triangulatedPoints = triangulatedPoints.T
-                triangulatedPoints = np.multiply(triangulatedPoints, rec_mask)
-                triangulatedPoints = triangulatedPoints[triangulatedPoints[:, 0] != 0]
+                triangulated_points = np.multiply(np.transpose(pose[4] / pose[4][3]), recover_pose_mask)
+                triangulated_points = triangulated_points[triangulated_points[:, 0] != 0]
 
-                if len(triangulatedPoints) > 7:
-                    cur_pose = np.matmul(cur_pose, np.linalg.inv(transf))
+                # If at least 7 good landmarks have been triangulated, calculate new pose
+                if len(triangulated_points) > 7:
+                    current_pose = np.matmul(current_pose, np.linalg.inv(transform_matrix))
+                    if(not found_enough_keypoints):
+                        print("Found enough keypoints again. Continuing movement.")
+                        found_enough_keypoints = True
                     # This part could be used for SLAM
-                    # for r, row in enumerate(triangulatedPoints):
-                    #     point = cur_pose@row.T
-                    #     triangulatedPoints[r] = point.T
-                    #
-                    # triangulatedPoints = triangulatedPoints[:,:3].astype('double')
-                    # q_last = q_last.astype('double')
-                    # self.q_current = self.q_current.astype('double')
-                    #
-                    # if triangulatedPoints.shape[0] >= 4:
-                    #     _, rvec, tvec, _ = cv2.solvePnPRansac(triangulatedPoints, self.q_current, self.K, None)
-                    #     R, _ = cv2.Rodrigues(rvec)
-                    #     back_pose = np.hstack((R, tvec))
-                    #     back_pose = np.r_[back_pose, [np.array([0, 0, 0, 1])]]
-                    #     back_pose = np.linalg.inv(back_pose)
-                    #
-                    #     # Filter out heavy outliers
-                    #     if abs(back_pose[0, 3]) + abs(back_pose[1, 3]) + abs(back_pose[2, 3]) < 1000:
-                    #         self.all_b_pose.append([back_pose[0, 3], back_pose[1, 3], back_pose[2, 3]])
-                    # else:
-                    #     print("No")
-                else:
-                    base = np.hstack((np.eye(3), np.zeros((3, 1))))
-                    base = np.r_[base, [np.array([0, 0, 0, 1])]]
-                    cur_pose = np.matmul(cur_pose, base)
-                    print("No move")
+                    #self.__slam(triangulated_points, current_pose)
 
+                # Else, assume we are not moving -> stay and don't update current_pose
+                elif(found_enough_keypoints):
+                    print("Not enough triangulated points. Assuming no movement.")
+                    found_enough_keypoints = False
 
-
-            self.all_path.append([cur_pose[0, 3], cur_pose[1, 3], cur_pose[2, 3]])
+            self.all_path.append([current_pose[0, 3], current_pose[1, 3], current_pose[2, 3]])
             if self.plot_live:
-                kk = np.array(self.all_path)
-                self.line1 = self.plotter.live_plotter(i,self.images[i],self.q_initial,self.q_current,kk[:,0], kk[:,2],self.line1)
+                updated_path = np.array(self.all_path)
+                self.line1 = self.plotter.live_plotter(i,self.images[i], self.q_initial, self.q_current, updated_path[:,0], updated_path[:,2], self.line1)
 
         ## Plot
         if self.plot_live:
             plt.ioff()
             plt.show()
+
+    def __slam(self, points, pose):
+        for r, row in enumerate(points):
+            point = pose@row.T
+            points[r] = point.T
+        
+        points = points[:,:3].astype('double')
+        q_last = q_last.astype('double')
+        self.q_current = self.q_current.astype('double')
+        
+        if points.shape[0] >= 4:
+            _, rvec, tvec, _ = cv2.solvePnPRansac(points, self.q_current, self.K, None)
+            R, _ = cv2.Rodrigues(rvec)
+            back_pose = np.hstack((R, tvec))
+            back_pose = np.r_[back_pose, [np.array([0, 0, 0, 1])]]
+            back_pose = np.linalg.inv(back_pose)
+        
+            # Filter out heavy outliers
+            if abs(back_pose[0, 3]) + abs(back_pose[1, 3]) + abs(back_pose[2, 3]) < 1000:
+                self.all_b_pose.append([back_pose[0, 3], back_pose[1, 3], back_pose[2, 3]])
